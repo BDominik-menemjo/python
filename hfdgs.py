beb81ec469065d4d0189043df1f47faca79e62e5
BDominik-menemjo <77264537+BDominik-menemjo@users.noreply.github.com>
2021-11-25 14:08:43 +0100

    Add files via upload

---
+++ hfdgs.py
@@ -0,0 +1,14 @@
+# listák
+
+lista=['alma', 'körte', 'bicska', 'pálinka', 'kerítésszaggató']
+print(lista)
+ujlista=[2, 'pitypang', 3.18, 'Freddy', 'Hey Google', True, ]
+print(ujlista)
+print(lista[2])
+
+#Slice roatation vagy szeletelő jelölés:
+print(lista[:3]) #A másodiktól a 3-ig. A 3. már nincs benne.
+print(lista[1:4]) #a másodiktól a 4-ig. A 5. már nincs benne.
+print(lista[-1]) #az utolsó elem indexe -1.
+print(list[-4]) #Hárultól a 4. elem
+print(lista[1:-1]) #A másodiktól az urolsó elemig.
