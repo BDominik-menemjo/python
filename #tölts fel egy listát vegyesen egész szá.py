a529b6e6d910ae96cc36cf35e3be57d715450708
BDominik-menemjo <77264537+BDominik-menemjo@users.noreply.github.com>
2021-12-02 14:01:39 +0100

    Add files via upload

--- #tölts fel egy listát vegyesen egész szá.py
+++ #tölts fel egy listát vegyesen egész szá.py
@@ -2,9 +2,10 @@
 #vagy irasd ki a listából csak az egész számokat. Az adatokat futási időben kell megadni.
 
 lista=[]
+ujlista=[]
 
-for i in range(10):
-    lista.append(input("Adj meg egy elemet:"))
+for i in range(5):
+    lista.append(input("Adj meg egy értéket:"))
 
-ujlista=[x for x in lista if type(x)==int]
+ujlista= [x for x in lista if x.isnumeric()==True]
 print(ujlista)
